8898223a6466ae4c3f2181b606e4208ed857abbd
cintiags14 <c.galvaogs@gmail.com>
2023-06-27 19:41:11 -0300

    Introdução e logica de programação

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # javascript
+
+JavaScript(ES6+), lógica de prog., orientação a objetos, criação de projetos para web com Node.js e Express
--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Heloow world java script")
+/*console.log(typeof (23 + 1));
+console.log(typeof (4 * 4));
+console.log(typeof (20 / 2));
+console.log(typeof (40 % 2));
+console.log(typeof (23 - 1));
+console.log("Primeira linha \n segunda linha");
+console.log(`a soma de 1 mais 1 é ${1+1}`);*/
+
+
+
+console.log("teste");
+cosole.log('teste2');
+cosole.log(`teste`);
